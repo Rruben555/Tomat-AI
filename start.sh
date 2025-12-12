926fccc812929865323c3b098c072b92af7f3354
Rruben555 <rubensiregar25@gmail.com>
2025-12-12 23:32:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+
+MODEL_DIR="model"
+MODEL_FILE="model/tomato_model.h5"
+MODEL_URL="https://drive.google.com/file/d/1d50zWVCxGQYEhBEVv9h523JytnrZo6Tx/view?usp=sharing"
+
+mkdir -p $MODEL_DIR
+
+# Download model jika belum ada
+if [ ! -f "$MODEL_FILE" ]; then
+    echo "Model not found. Downloading..."
+    wget -O "$MODEL_FILE" "$MODEL_URL"
+else
+    echo "Model already exists. Skipping download."
+fi
+
 gunicorn app:app --bind 0.0.0.0:$PORT
