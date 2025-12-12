25233a5da29eca1d67f02ac81db1df32d967e1b1
Rruben555 <rubensiregar25@gmail.com>
2025-12-12 23:41:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 MODEL_PATH="model/tomato_model.h5"
-MODEL_URL="https://drive.google.com/uc?id=1d50zWVCxGQYEhBEVv9h523JytnrZo6Tx"
+MODEL_URL="https://drive.google.com/uc?id=1d50zWVCxGQYEhBEVv9h523JytnrZo6Tx&export=download"
 
 if [ ! -f "$MODEL_PATH" ]; then
     echo "Model not found. Downloading..."
     mkdir -p model
     gdown "$MODEL_URL" -O "$MODEL_PATH"
+
+    echo "Downloaded file info:"
+    ls -lh "$MODEL_PATH"
 fi
 
 echo "Starting server..."
